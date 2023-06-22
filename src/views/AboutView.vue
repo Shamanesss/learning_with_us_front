c67b3474f2b382b48ee34114481f60fe4cd0c59d
Shamanesss <mentulbenodorna@gmail.com>
2023-06-22 23:56:12 +0200

    error

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>Formularios</h1>
+    <div class="container">
+      <FormLecciones />
+      <FormCursos />
+    </div>
   </div>
 </template>
+
+<script setup>
+import FormCursos from "@/components/Formularios/FormCursos.vue";
+import FormLecciones from "../components/Formularios/FormLecciones.vue";
+</script>
+
+<style>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+}
+</style>
